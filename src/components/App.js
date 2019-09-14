3417c5c6173af64d9cfdf1948519ed2feb25d839
Sarthak Agarwal <sarthak.agarwal97@gmail.com>
2019-09-15 02:30:11 +0530

    final bug fixed

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ class App extends Component{
 
         return(
             <div>
-            <h1>kfhkdjhgkjdh</h1>
+            <h1>Welcome to My React Boilerplate</h1>
             </div>
         );
     }
